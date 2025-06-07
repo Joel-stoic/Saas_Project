42624272a7e776cb127defa09118e88bf31e2bbe
Joel-stoic <joelimmanual.off@gmail.com>
2025-06-07 21:02:52 +0530

    Worked on pricing-section

--- components/common/how-it-works-section.tsx
+++ components/common/how-it-works-section.tsx
@@ -27,7 +27,7 @@ const step:Step[]=[
 
 export default function HowItWorksSection() {
     return (
-        <section className="relative overflow-hidden bg-gray-50">
+        <section className="relative overflow-hidden bg-gradient-to-r from-gray-200 to-gray-50  ">
             <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 ">
                 <div
                     aria-hidden="true"
@@ -41,7 +41,7 @@ export default function HowItWorksSection() {
                         }}
                     />
                 </div>
-                <div className="text-center mb-16 ">
+                <div className="text-center mb-16  ">
                     <h2 className="text-xl uppercase mb-4 font-bold text-rose-500">How it works</h2>
                     <h3 className="font-bold text-3xl max-w-2xl mx-auto">Transform any PDF into an easy-to-digest summary in three simple steps</h3>
                     <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl mx-auto relative">
--- components/home/pricing-section.tsx
+++ components/home/pricing-section.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { Check, ArrowRight } from "lucide-react";
+import { Check, ArrowRight, CheckIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 type PriceType = {
@@ -42,7 +42,7 @@ const plans: PriceType[] = [
 
 const PricingCard = ({ name, price, description, items, paymentLink, id }: PriceType) => {
     return (
-        <div className="relative w-full max-w-lg">
+        <div className="relative w-full max-w-lg hover:scale-105 hover:transition-all hover:duration-300">
          <div className={cn("relative flex flex-col h-full gap-4 lg:gap-8 z-10 p-8  border-[1px] border-gray-500/20 rounded-2xl", id === 'pro' && 'border-rose-500 gap-5 border-2')}> 
             <div className="flex justify-between items-center gap-4">
                 <div>
@@ -50,16 +50,23 @@ const PricingCard = ({ name, price, description, items, paymentLink, id }: Price
                     <p className="text-base-content/80 mt-2">{description}</p>
                 </div>
             </div>
-            <div>
-                <p>{price}</p>
+            <div className="flex gap-2">
+                <p className="text-5xl tracking-tight font-extrabold ">{price}</p>
+                <div className="flex flex-col justify-end mb-[4px]">
+                    <p className="text-xs uppercase font-semibold">USD</p>
+                    <p className="text-xs">/month</p>
+                </div>
             </div>
-            <div>
+            <div className="space-y-2.5 leading-relaxed text-base flex-1">
                 {items.map((item, idx) => (
-                    <li key={idx}>{item}</li>
+                    <li key={idx} className="flex items-center gap-2">
+                        <CheckIcon size={18}/>
+                        <span>{item}</span>
+                     </li>
                 ))}
             </div>
-            <div>
-                <Link href={paymentLink}>Buy Now</Link>
+            <div className="space-y-2 flex justify-center w-full"> 
+                <Link href={paymentLink} className={cn("w-full rounded-full flex items-center justify-center gap-2 bg-linear-to-r from-rose-800 to-rose-500 hover:from-rose-500 hover:to-rose-800 text-white border-2 py-2",id === 'pro'? 'border-rose-900':'border-rose-100 from-rose-400 to-rose-500')}>Buy Now <ArrowRight size={18}/></Link>
             </div>
          </div>
         </div>
@@ -68,10 +75,10 @@ const PricingCard = ({ name, price, description, items, paymentLink, id }: Price
 
 export default function PricingSection(){
     return (
-        <section className="">
-            <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px:6 lg:px-8 lg:pt-12">
-                <div className="">
-                    <h2>Pricing</h2>
+        <section className="relative overflow-hidden id" id='pricing'>
+            <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px:6 lg:px-8 lg:pt-12 flex flex-col gap-8 ">
+                <div className="flex items-center justify-center w-full pb-12">
+                    <h2 className="text-rose-500 font-bold text-3xl uppercase mb-8">Pricing</h2>
                 </div>
                 <div className="relative flex justify-center flex-col lg:flex-row items-center lg:items-stretch gap-8">
                     {plans.map((plan)=><PricingCard key={plan.id} {...plan}/>)}
@@ -80,3 +87,4 @@ export default function PricingSection(){
         </section>
     )
 }
+
